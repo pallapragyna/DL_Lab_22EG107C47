112630eb40a0623466de7bba62585647637d835f
pallapragyna <pallapragyna@gmail.com>
2025-07-28 20:17:51 +0530

    first commit

--- cifar_mlp.py
+++ cifar_mlp.py
@@ -16,8 +16,8 @@ y_test = to_categorical(y_test)
 #build the architecture
 model = Sequential()
 model.add(Flatten(input_shape=(32,32,3)))
-model.add(Dense(units=10,activation='relu'))
-model.add(Dense(units=10,activation='softmax'))
+model.add(Dense(units=8,activation='relu'))
+model.add(Dense(units=8,activation='softmax'))
 
 #compile
 model.compile(optimizer='adam',loss= 'categorical_crossentropy',metrics=['accuracy'])
